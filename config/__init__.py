54ec3c6a71623cd974d23c3bcce87d66d8eaa85a
esmond <dhonniemccony3@gmail.com>
2026-06-10 11:58:19 -1200

    updated config

--- config/__init__.py
+++ config/__init__.py
@@ -1,3 +1,14 @@
+from django.template.context import BaseContext
+
+
+def _patched_basecontext_copy(self):
+    duplicate = self.__class__.__new__(self.__class__)
+    duplicate.__dict__.update(self.__dict__)
+    duplicate.dicts = self.dicts[:]
+    return duplicate
+
+BaseContext.__copy__ = _patched_basecontext_copy
+
 from .celery import app as celery_app
 
 __all__ = ('celery_app',)
